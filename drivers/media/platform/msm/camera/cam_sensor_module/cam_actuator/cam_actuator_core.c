4f60116731cdac12b54e3460d11183b345c409ff
kdrag0n <dragon@khronodragon.com>
2019-01-10 22:01:29 -0800

    msm: camera: boost CPU and DDR for 500 ms on camera init
    Speeds up camera opening.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/media/platform/msm/camera/cam_sensor_module/cam_actuator/cam_actuator_core.c
+++ drivers/media/platform/msm/camera/cam_sensor_module/cam_actuator/cam_actuator_core.c
@@ -11,6 +11,7 @@
  */
 
 #include <linux/module.h>
+#include <linux/devfreq_boost.h>
 #include <cam_sensor_cmn_header.h>
 #include "cam_actuator_core.h"
 #include "cam_sensor_util.h"
@@ -795,6 +796,9 @@ int32_t cam_actuator_i2c_pkt_parse(struct cam_actuator_ctrl_t *a_ctrl,
 
 	switch (csl_packet->header.op_code & 0xFFFFFF) {
 	case CAM_ACTUATOR_PACKET_OPCODE_INIT:
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 500);
+		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 500);
+
 		offset = (uint32_t *)&csl_packet->payload;
 		offset += (csl_packet->cmd_buf_offset / sizeof(uint32_t));
 		cmd_desc = (struct cam_cmd_buf_desc *)(offset);
