8068ad14193e66420fc75372e3796c54faa76e2d
viciouspup <yadhuuk@gmail.com>
2021-03-26 11:36:39 +0000

    drivers/devfreq/devfreq_devbw: fix compiling error

--- drivers/devfreq/devfreq_devbw.c
+++ drivers/devfreq/devfreq_devbw.c
@@ -213,13 +213,14 @@ int devfreq_add_devbw(struct device *dev)
 		return PTR_ERR(d->df);
 	}
 
-	if (!strcmp(dev_name(dev), "soc:qcom,cpu-llcc-ddr-bw"))
+	if (!strcmp(dev_name(dev), "soc:qcom,cpu-llcc-ddr-bw")) {
 		devfreq_register_boost_device(DEVFREQ_MSM_CPUBW, d->df);
+		pr_info("Added devfreq boost device %s\n",dev_name(dev));
 		}
 	if (!strcmp(dev_name(dev), "soc:qcom,gpubw")) {
 		devfreq_register_boost_gpu_device(DEVFREQ_MSM_GPUBW, d->df);
 		pr_info("Added devfreq gpu boost device %s\n",dev_name(dev));
-	}
+	    }
 	
 	return 0;
 }
