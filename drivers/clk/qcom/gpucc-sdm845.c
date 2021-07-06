ed131d228f9dd8edfe0ff0271ab6c86e0ab5c051
viciouspup <yadhuuk@gmail.com>
2021-07-06 06:40:03 +0000

    bitxo valur changed

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -225,7 +225,7 @@ static const struct freq_tbl ftbl_gpu_cc_gmu_clk_src_sdm845_v2[] = {
 };
 
 static const struct freq_tbl ftbl_gpu_cc_gmu_clk_src_sdm670[] = {
-	F(19200000, P_BI_TCXO, 1, 0, 0),
+	F(20000000, P_BI_TCXO, 1, 0, 0),
 	F(200000000, P_GPLL0_OUT_MAIN_DIV, 1.5, 0, 0),
 	{ }
 };
